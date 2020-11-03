118bc2f47b28e2c3dda68230468b151400f08740
wprzewoznik <w.przewoznik@gmail.com>
2020-11-03 23:44:13 +0100

    Dodanie opisu danych

---
+++ R/data.R
@@ -0,0 +1,32 @@
+#' Wskazniki emisji z transportu drogowego EEA
+#'
+#' @source MS Teams zajec
+#'
+#' @format Obiekt zawiera
+#' \describe{
+#'  \item{Category}{Kategoria pojazdu}
+#'  \item{Fuel}{Rodzaj paliwa}
+#'  \item{Segment}{Segment}
+#'  \item{Euro.Standard}{Standard emisji EURO}
+#'  \item{Technology}{Technologia}
+#'  \item{Pollutant}{Zanieczyszczenie}
+#'  \item{Mode}{Tryb}
+#'  \item{Alpha}{Parametr potrzebny od obliczen}
+#'  \item{Beta}{Parametr potrzebny od obliczen}
+#'  \item{Gamma}{Parametr potrzebny od obliczen}
+#'  \item{Delta}{Parametr potrzebny od obliczen}
+#'  \item{Epsilon}{Parametr potrzebny od obliczen}
+#'  \item{Zita}{Parametr potrzebny od obliczen}
+#'  \item{Hta}{Parametr potrzebny od obliczen}
+#'  \item{Reduction}{Redukcja}
+#'  \item{Bio}{Wskaznik BIO}
+#'  \item{Procent}{Procent}
+#' }
+#'
+#' @examples
+#' \dontrun{
+#'  wskazniki
+#'  input
+#' }
+"wskazniki"
+"input"
